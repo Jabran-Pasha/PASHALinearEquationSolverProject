1ad9519fad77567cf2402028180386d4be0ebb4c
Jabran Pasha <jabranp2@nycstudents.net>
2023-10-29 16:14:40 -0400

    Initial commit

---
+++ src/LinearEquationRunner.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class LinearEquationRunner {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        //Start & Initial Inputs
+        System.out.println("Welcome!");
+
+        System.out.println("Enter coordinate 1: ");
+        String coord1 = scan.nextLine();
+
+        System.out.println("Enter coordinate 2: ");
+        String coord2 = scan.nextLine();
+
+        System.out.println("The two points are " + coord1 + " and " + coord2);
+
+        //Using parseInt method to convert input into x and y coordinates
+        //Using the indexOf and substring methods to correctly parse coord1 and coord2
+        //coord1
+        int x1 = Integer.parseInt(coord1.substring(coord1.indexOf("(") + 1, (coord1.indexOf(","))));
+        int y1 = Integer.parseInt(coord1.substring(coord1.indexOf(",")+ 2, (coord1.indexOf(")"))));
+
+        //coord2
+        int x2 = Integer.parseInt(coord2.substring(coord2.indexOf("(") + 1, (coord2.indexOf(","))));
+        int y2 = Integer.parseInt(coord2.substring(coord2.indexOf(",")+ 2, (coord2.indexOf(")"))));
+
+
+
+
+
+
+    }
+}
